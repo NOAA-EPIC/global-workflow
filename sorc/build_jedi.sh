b266045e7ee8e5986af2a9d3d725ff178fd9ab52
Mark A Potts <Mark.Potts@noaa.gov>
2025-01-30 18:27:15 +0000

    Updates to build with uwtools

---
+++ sorc/build_jedi.sh
@@ -0,0 +1,30 @@
+#! /usr/bin/env bash
+set -eux
+
+OPTIND=1
+_opts="-f "  # forces a clean build
+while getopts ":j:idv" option; do
+  case "${option}" in
+    d) _opts+="-c -DCMAKE_BUILD_TYPE=Debug " ;;
+    j) BUILD_JOBS=${OPTARG};;
+    v) _opts+="-v ";;
+    i) _opts+="-i -a ";;
+    :)
+      echo "[${BASH_SOURCE[0]}]: ${option} requires an argument"
+      usage
+      ;;
+    *)
+      echo "[${BASH_SOURCE[0]}]: Unrecognized option: ${option}"
+      usage
+      ;;
+  esac
+done
+shift $((OPTIND-1))
+
+# double quoting opts will not work since it is a string of options
+# shellcheck disable=SC2086
+BUILD_JOBS="${BUILD_JOBS:-8}" \
+WORKFLOW_BUILD="ON" \
+./gdas.cd/build.sh ${_opts} -f
+
+exit
